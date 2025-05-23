9c2d6df06b3dd90dd8120f92e6b0cc459b5bb08f
zaidanav <13522146@std.stei.itb.ac.id>
2025-05-23 19:07:50 +0700

    ilang

--- app/src/main/java/com/example/purrytify/models/LoginResponse.kt
+++
@@ -1,5 +0,0 @@
-package com.example.purrytify.models
-
-data class LoginResponse(
-    val accessToken: String,
-)
--- app/src/main/java/com/example/purrytify/models/Songs.kt
+++
@@ -1,7 +0,0 @@
-package com.example.purrytify.models
-
-data class Songs(
-    val title: String,
-    val artist: String,
-    val imageResId: Int
-)
